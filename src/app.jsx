ca6020d3f2c303557829d117a3dc6705e9775c1b
panjiajie <251907882@qq.com>
2020-07-09 11:47:22 +0800

    依赖

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,11 @@
 import Taro, { Component } from '@tarojs/taro'
 import Index from './pages/index'
-
-import './app.less'
-
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
 // if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
 //   require('nerv-devtools')
 // }
-
 class App extends Component {
-
-  componentDidMount () {}
-
-  componentDidShow () {}
-
-  componentDidHide () {}
-
-  componentDidCatchError () {}
-
   config = {
     pages: [
       'pages/index/index'
@@ -30,7 +17,6 @@ class App extends Component {
       navigationBarTextStyle: 'black'
     }
   }
-
   // 在 App 类中的 render() 函数没有实际作用
   // 请勿修改此函数
   render () {
@@ -39,5 +25,4 @@ class App extends Component {
     )
   }
 }
-
 Taro.render(<App />, document.getElementById('app'))
